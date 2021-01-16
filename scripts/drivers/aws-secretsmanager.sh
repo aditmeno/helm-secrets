a4db3a09a7db73e8b3fd90d80bb4e62aa3663891
Aditya Menon <aditya.menon@spin.pm>
2021-01-16 17:25:17 +0530

    Pull out common code to a fucntion

--- scripts/drivers/aws-secretsmanager.sh
+++ scripts/drivers/aws-secretsmanager.sh
@@ -6,11 +6,7 @@ _DRIVER_REGEX='aws-secretsmanager [A-z0-9][A-z0-9/\-]*\#[A-z0-9][A-z0-9-]*'
 # shellcheck source=scripts/drivers/_custom.sh
 . "${SCRIPT_DIR}/drivers/_custom.sh"
 
-_custom_driver_get_secret() {
-    _type=$1
-    _SECRET_ID="${2%#*}"
-    _SECRET_KEY="${2#*#}"
-
+common_codebase() {
     if [ "${_type}" != "yaml" ]; then
         echo "Only decryption of yaml files are allowed!"
         exit 1
@@ -48,6 +44,14 @@ _custom_driver_get_secret() {
 
         fi
     fi
+}
+
+_custom_driver_get_secret() {
+    _type=$1
+    _SECRET_ID="${2%#*}"
+    _SECRET_KEY="${2#*#}"
+
+    common_codebase
 
     if ! aws secretsmanager get-secret-value --secret-id ${_SECRET_ID} --output json | jq --raw-output '.SecretString' | jq -r .${_SECRET_KEY}; then
         echo "Error while get secret from aws secrets manager!" >&2
@@ -66,48 +70,12 @@ driver_decrypt_file() {
     _input=${2}
     _output=${3}
 
-    if [ "${_type}" != "yaml" ]; then
-        echo "Only decryption of yaml files are allowed!"
-        exit 1
-    fi
-
-    if ! aws sts get-caller-identity --output text >/dev/null; then
-        if [ -z ${AWS_SECRET_ACCESS_KEY} && -z ${AWS_ACCESS_KEY_ID} ]; then
-            if [ -z ${AWS_ROLE_ARN} && -z ${AWS_WEB_IDENTITY_TOKEN_FILE} ]; then
-                echo "Missing AWS Credential Environment Variables!"
-                exit 1
-            fi
-        fi
-    fi
-
-    if ! aws sts get-caller-identity --output text >/dev/null; then
-        if [ -z ${AWS_DEFAULT_REGION} || -z ${AWS_REGION} ]; then
-            echo "Missing AWS Region where the Secrets exist"
-            exit 1
-        fi
-    fi
-
-    if ! aws sts get-caller-identity --output text >/dev/null; then
-        if [ ! -z ${AWS_ROLE_ARN} || ! -z ${AWS_WEB_IDENTITY_TOKEN_FILE} ]; then
-            aws sts assume-role-with-web-identity \
-                --role-arn $AWS_ROLE_ARN \
-                --role-session-name x-account \
-                --web-identity-token file://$AWS_WEB_IDENTITY_TOKEN_FILE \
-                --duration 1500 >/tmp/temp_creds.txt
-
-            export AWS_ACCESS_KEY_ID="$(cat /tmp/temp_creds.txt | jq -r ".Credentials.AccessKeyId")"
-            export AWS_SECRET_ACCESS_KEY="$(cat /tmp/temp_creds.txt | jq -r ".Credentials.SecretAccessKey")"
-            export AWS_SESSION_TOKEN="$(cat /tmp/temp_creds.txt | jq -r ".Credentials.SessionToken")"
-
-            rm /tmp/temp_creds.txt
-
-        fi
-    fi
+    common_codebase
 
     export FILE_INPUT_PATH=${_input}
     export FILE_OUTPUT_PATH=${_output}
 
-    python3 << EOF
+    python3 <<EOF
 import yaml
 import boto3
 import json
@@ -137,5 +105,8 @@ with open(os.environ["FILE_INPUT_PATH"]) as file:
 with open(os.environ["FILE_OUTPUT_PATH"], "w") as file:
     documents = yaml.dump(parsed_secrets, file)
 EOF
+
+    unset FILE_INPUT_PATH
+    unset FILE_OUTPUT_PATH
 
 }
